1c627d2dafa313e44bb7e1440cc076a1c33382f7
TheDawgry <ircurlicus123@gmail.com>
2019-01-04 18:24:44 +0100

    Updated time

--- playground.swift
+++ playground.swift
@@ -3,7 +3,7 @@ import Foundation
 class ConferenceRoom{
     var name: String
     var state: Bool
-    var time: Int8
+    var time: String
 
     func description() {
         print(name, state, separator: " - ", terminator: "")
@@ -14,7 +14,8 @@ class ConferenceRoom{
         }
     }
 
-    init(name: String, state: Bool, time: Int8) {
+    init(name: String, state: Bool, time: String) {
+
         self.name = name
         self.state = state
         self.time = time
@@ -22,21 +23,21 @@ class ConferenceRoom{
     init(name: String, state: Bool) {
         self.name = name
         self.state = state
-        self.time = 0
+        self.time = ""
     }
 }
 
 var room: [ConferenceRoom] = []
-room.append(ConferenceRoom(name: "Sala 1",state: true,time: 15))
-room.append(ConferenceRoom(name: "Sala 2",state: true,time: 10))
+room.append(ConferenceRoom(name: "Sala 1",state: true,time: "07/01/2019 11:45"))
+room.append(ConferenceRoom(name: "Sala 2",state: true,time: "07/01/2019 20:15"))
 room.append(ConferenceRoom(name: "Sala 3",state: false))
-room.append(ConferenceRoom(name: "Sala 4",state: true,time: 3))
+room.append(ConferenceRoom(name: "Sala 4",state: true,time: "01/02/2019 12:00"))
 room.append(ConferenceRoom(name: "Sala 5",state: false))
 room.append(ConferenceRoom(name: "Sala 6",state: false))
-room.append(ConferenceRoom(name: "Sala 7",state: true,time: 7))
-room.append(ConferenceRoom(name: "Sala 8",state: true,time: 12))
+room.append(ConferenceRoom(name: "Sala 7",state: true,time: "06/01/2019 15:30"))
+room.append(ConferenceRoom(name: "Sala 8",state: true,time: "21/01/2019 10:30"))
 room.append(ConferenceRoom(name: "Sala 9",state: false))
-room.append(ConferenceRoom(name: "Sala 10",state: true,time: 5))
+room.append(ConferenceRoom(name: "Sala 10",state: true,time: "04/01/2019 23:00"))
 
 var freeRoom: [ConferenceRoom] = []
 var takenRoom: [ConferenceRoom] = []
